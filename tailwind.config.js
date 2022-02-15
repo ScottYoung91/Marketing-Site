c19d11df82bd551b31777d38f6a6b5029604fd6f
ScottYoung91 <32314440+ScottYoung91@users.noreply.github.com>
2022-02-14 19:23:01 -0600

    Revert "update styles"
    This reverts commit f4da0c3ab8bec7d493b98fb0b84ea751e2dda985.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@ module.exports = {
 	purge: { enabled: true, content: ['./*.html'] },
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {
-			colors: {
-				accent: '#0cfab7',
-				accentLight: '#85fcdb',
-				'accent-2': '#e0082d',
-				accent2Light: '#ef8396',
-			},
-			html: {
-				'scroll-behavior': 'smooth',
-			},
-		},
+		extend: {},
 	},
 	variants: {
 		extend: {},
