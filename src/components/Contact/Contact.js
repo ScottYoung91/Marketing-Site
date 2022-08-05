1a10e8a6f5c6188f3167487abf71f609675061d0
ScottYoung91 <32314440+ScottYoung91@users.noreply.github.com>
2022-08-04 20:10:58 -0500

    netlify form bug fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,17 +5,23 @@ const Contact = () => {
 	return (
 		<section className='contact'>
 			<div className='container'>
-				<form action='' className='contact-form' name='contact ' netlify>
+				<form
+					action=''
+					className='contact-form'
+					name='contact '
+					netlify
+					netlify-honeypot='bot-field'
+				>
 					<h4>Contact Us</h4>
 					<input type='text' name='name' id='name' placeholder='name' />
 					<input type='text' name='email' id='email' placeholder='email' />
-					<input
+					<textarea
 						type='text'
 						name='description'
 						id='description'
 						placeholder='description'
 						className='description'
-					/>
+					></textarea>
 					<button type='submit'>Submit</button>
 				</form>
 			</div>
